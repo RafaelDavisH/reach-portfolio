8208fe1431cf03b9d058e61dfc498d6d64b22adb
RafaelDavisH <rafaeldavish443@gmail.com>
2019-05-14 00:35:27 -0700

    Updates

---
+++ precache-manifest.e3b2dbfcc7f1cf67554c0f927f2c4680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cec7349659d75768fd522ed4190a71",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "24275f411fc2bcb768b9",
+    "url": "/react-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2c316c54ea2c2eb61611",
+    "url": "/react-portfolio/static/js/2.5bf9cfb2.chunk.js"
+  },
+  {
+    "revision": "24275f411fc2bcb768b9",
+    "url": "/react-portfolio/static/js/main.85988b9f.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "df4eacf086b1e940b9bd717b39788262",
+    "url": "/react-portfolio/static/media/webdevResume.df4eacf0.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RafaelDavisH/react-portfolio/precache-manifest.a0ac16817e503ae3c114909548d66f1b.js"
+  "/react-portfolio/precache-manifest.e3b2dbfcc7f1cf67554c0f927f2c4680.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RafaelDavisH/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
